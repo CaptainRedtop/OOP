c248ffed6798bd0617b562bc7f61b0f209d5f568
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-09-11 11:40:56 +0200

    Add files via upload

---
+++ Superhero/Superhero/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Superhero
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			SuperPerson superPerson1 = new SuperPerson();
+
+			superPerson1.PublicName = "Clark Joseph Kent";
+			superPerson1.CivilJob = "Journalist";
+			superPerson1.Powers = "Flight";
+			superPerson1.Costume = "Spandex and cape";
+			superPerson1.ChildhoodDrama = "Lost parents, race and planet";
+			superPerson1.Hero = true;
+
+			Console.Write(superPerson1.PublicName);
+			Console.ReadKey();
+			
+		}
+	}
+}
---
+++ Superhero/Superhero/SuperPerson.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero
+{
+	public class SuperPerson
+	{
+		private string _publicName;
+		private string _powers;
+		private string _costume;
+		private string _civilJob;
+		private string _childhoodDrama;
+		private bool _hero;
+
+		public string PublicName
+		{ 
+			get { return _publicName; } 
+			set { _publicName = value; }
+		}
+		public string Powers
+		{
+			get { return _powers; }
+			set { _powers = value; }
+		}
+		public string Costume
+		{
+			get { return _costume; }
+			set { _costume = value; }
+		}
+		public string CivilJob
+		{
+			get { return _civilJob; }
+			set { _civilJob = value; }
+		}
+		public string ChildhoodDrama
+		{
+			get { return _childhoodDrama; }
+			set { _childhoodDrama = value; }
+		}
+		public bool Hero
+		{
+			get { return _hero; }
+			set { _hero = value; }
+		}
+
+
+	}
+}
