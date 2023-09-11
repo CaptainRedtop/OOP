7196f5e9105432b5e38e90cb14bacad7756d792d
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-09-11 13:02:13 +0200

    Add files via upload

---
+++ Elon's_New_Venture/Elon's_New_Venture/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Elon_s_New_Venture
+{
+	internal class Program
+	{
+		private void Main(string[] args)
+		{
+			StartController();
+		}
+
+		#region GUI
+		private System.ConsoleKeyInfo MainMenu()
+		{
+			Console.WriteLine("--------------------------------");
+			Console.WriteLine("-------------RC Car-------------");
+			Console.WriteLine("--------------------------------");
+			Console.WriteLine("\n\n\nWhat would you like to do?\n");
+			Console.WriteLine("1. Create a new RC car.");
+			Console.WriteLine("2. Drive a car.");
+			Console.WriteLine("3. View a list of cars.");
+			Console.WriteLine("4. Charge a cars battery");
+			var key = Console.ReadKey();
+			return key;
+		}
+		#endregion GUI
+
+		#region Controller
+		private void StartController()
+		{
+			switch (MainMenu())
+			{
+				case (ConsoleKey.D1):
+				{
+					break;
+				}
+			}
+		}
+		#endregion Controller
+
+		#region Model
+
+		#endregion Model
+	}
+}
---
+++ Elon's_New_Venture/Elon's_New_Venture/RemoteControlCar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elon_s_New_Venture
+{
+	public class RemoteControlCar
+	{
+		private string _carColor;
+		private short _metersDriven = 0;
+		private byte _batteryCapacity = 100;
+
+		public string CarColor
+		{
+			get { return _carColor; }
+			set { _carColor = value; }
+		}
+
+		public short MetersDriven
+		{
+			get { return _metersDriven; }
+			set { _metersDriven = value; }
+		}
+		public byte BatteryCapacity
+		{
+			get { return (byte)_batteryCapacity; }
+			set { _batteryCapacity = value; }
+		}
+	}
+}
