fd27895a342643c8bd228879163d253f00a4eb19
CaptainRedtop <klich.niklas@hotmail.com>
2023-09-11 13:31:12 +0200

    Something

--- Elon's_New_Venture/Elon's_New_Venture/Program.cs
+++ Elon's_New_Venture/Elon's_New_Venture/Program.cs
@@ -4,40 +4,7 @@
 	{
 		private void Main(string[] args)
 		{
-			StartController();
+			RCController();
 		}
-
-		#region GUI
-		private System.ConsoleKeyInfo MainMenu()
-		{
-			Console.WriteLine("--------------------------------");
-			Console.WriteLine("-------------RC Car-------------");
-			Console.WriteLine("--------------------------------");
-			Console.WriteLine("\n\n\nWhat would you like to do?\n");
-			Console.WriteLine("1. Create a new RC car.");
-			Console.WriteLine("2. Drive a car.");
-			Console.WriteLine("3. View a list of cars.");
-			Console.WriteLine("4. Charge a cars battery");
-			var key = Console.ReadKey();
-			return key;
-		}
-		#endregion GUI
-
-		#region Controller
-		private void StartController()
-		{
-			switch (MainMenu())
-			{
-				case (ConsoleKey.D1):
-				{
-					break;
-				}
-			}
-		}
-		#endregion Controller
-
-		#region Model
-
-		#endregion Model
 	}
 }
