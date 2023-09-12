5a16661b6dfc1b1b3836926bcc83b6bc8098818b
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-09-12 10:54:52 +0200

    Added robot

---
+++ Robot/Robot/Model/Robot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot.Model
+{
+	public class Robot
+	{
+		public string _color;
+		public string _chip;
+		public bool _wiFi;
+		public bool _small;
+		public bool _battery;
+		
+		public Robot()
+		{ 
+
+		}
+		public Robot(bool small) : this()
+		{
+			_small = small;
+		}
+		public Robot(bool small, string chip) : this(small)
+		{
+			_chip = chip;
+		}
+		public Robot(bool battery, bool small, string chip) : this(small, chip)
+		{
+			_battery = battery;
+		}
+
+	}
+}
