eaaa446333d491744c9bd6a9b744ae6b1c7494ae
CaptainRedtop <134284651+CaptainRedtop@users.noreply.github.com>
2023-09-13 09:28:02 +0200

    Add files via upload

---
+++ SortingHat/SortingHat/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SortingHat
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				SortingHat sortingHat = new SortingHat();
+				Console.WriteLine("You belong to: \n");
+				sortingHat.House();
+				Console.WriteLine("\n\nDo you want to go again? y/n");
+				char choice = Convert.ToChar(Console.ReadLine().ToLower());
+				if(choice == 'y')
+				{
+					Console.Clear();
+				}
+				else if (choice == 'n')
+				{
+					break;
+				}
+				else
+				{
+					Console.WriteLine("Wrong input you donk!");
+					Thread.Sleep(5000);
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ SortingHat/SortingHat/SortingHat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingHat
+{
+	internal class SortingHat
+	{
+		enum Houses { Gryffinfor, Hufflepuff, Ravenclaw, Slytherin };
+
+		public int Random()
+		{
+			Random rand = new Random();
+			int number = rand.Next(1,5);
+			return number;
+		}
+
+		public void House()
+		{
+			int number = Random();
+
+			switch (number)
+			{
+				case 1:
+					{
+						Console.WriteLine(Houses.Gryffinfor);
+						break;
+					}
+				case 2:
+					{
+						Console.WriteLine(Houses.Hufflepuff);
+						break;
+					}
+				case 3:
+					{
+						Console.WriteLine(Houses.Ravenclaw);
+						break;
+					}
+				case 4:
+					{
+						Console.WriteLine(Houses.Slytherin);
+						break;
+					}
+			}
+		}
+
+	}
+}
